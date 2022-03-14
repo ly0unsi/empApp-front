f445ae75482213b8ae7b457b5cce6ae0ed6c63a1
ly0unsi <mcabdo25@gmail.com>
2022-03-14 14:00:57 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent implements OnInit{
   public employees: Employee[]; 
   public editEmployee:Employee;
+  public deleteEmployee:Employee;
   constructor(private empService:EmployeeService) { }
 
   ngOnInit(): void {
@@ -42,7 +43,45 @@ export class AppComponent implements OnInit{
       }
     );
   }
-  public onEditClick(emp:Employee){
-    this.editEmployee==emp;
+  public onEditEmployee(emp:Employee): void {
+
+    this.empService.editEmployee(emp).subscribe(
+      (response: Employee) => {
+        this.getEmployees();
+        
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  public onDeleteEmployee():void{
+    this.empService.deleteEmployee(this.deleteEmployee?.id).subscribe(
+      () => {
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  public searchEmployees(key:string):void{
+    var  results:Employee[]=[];
+    console.log(key);
+     for (const emp of this.employees) {
+       if(emp?.name.toLowerCase().indexOf(key.toLowerCase())!==-1 || emp?.phone.indexOf(key.toLowerCase())!==-1 || emp?.email.indexOf(key.toLowerCase())!==-1)
+        results.push(emp);
+     }
+     this.employees=results;
+     if (!key) {
+       this.getEmployees();
+     }
+  }
+  public onClick(emp:Employee,mod:string){
+    if (mod=="edit") {
+      this.editEmployee=emp;
+    }else{
+      this.deleteEmployee=emp;
+    }
   }
 }
